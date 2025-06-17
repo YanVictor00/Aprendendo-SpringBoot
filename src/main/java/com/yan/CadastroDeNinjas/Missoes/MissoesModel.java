9272a0621b39493173a4ecb18e570a87fb2983fb
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-06-17 20:25:41 -0300

    Utilizando LOMBOK

--- src/main/java/com/yan/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/yan/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.yan.CadastroDeNinjas.Missoes;
 
 import com.yan.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -19,34 +25,7 @@ public class MissoesModel {
     private List<NinjaModel> ninjas;
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getRank() {
-        return rank;
-    }
 
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public MissoesModel(Long id, String nome, String rank){
-        this.id = id;
-        this.nome = nome;
-        this.rank = rank;
-    }
-    public MissoesModel(){}
 }
--- src/main/java/com/yan/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/yan/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,17 @@ package com.yan.CadastroDeNinjas.Ninjas;
 
 import com.yan.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name ="tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -21,37 +27,7 @@ public class NinjaModel {
     private MissoesModel missoes;
 
 
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel(){
-
-    }
 }
