eb27f4746d78c461201897357d4a27770a945d4b
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-06-16 21:16:08 -0300

    vsf OneToMany e ManyToOne

--- src/main/java/com/yan/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/yan/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,6 +3,8 @@ package com.yan.CadastroDeNinjas.Missoes;
 import com.yan.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_missoes")
 public class MissoesModel {
@@ -12,9 +14,34 @@ public class MissoesModel {
     private Long id;
     private String nome;
     private String rank;
-    private NinjaModel ninja;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public MissoesModel(Long id, String nome, String rank){
         this.id = id;
