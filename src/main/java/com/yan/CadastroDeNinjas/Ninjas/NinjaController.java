94628fe238e5daa7e250d420d1b57c6eed8f4412
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-06-29 11:41:16 -0300

    Chore: .gitignore iclui o ./data

--- src/main/java/com/yan/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/yan/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,7 +1,5 @@
 package com.yan.CadastroDeNinjas.Ninjas;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
